b9ad383144db00b5b5548b83ea3d699b8945ca7f
juyeon99 <beive999@naver.com>
2024-09-11 15:05:52 +0900

    [FEAT] footer 추가
    footer 추가

--- src/components/commons/Footer.js
+++ src/components/commons/Footer.js
@@ -1,6 +1,14 @@
+import "./Footer.css";
+
 function Footer() {
     return (
-        <h1>Ohdaesan Inc.</h1>
+        <div id="footer">
+            <b>Ohdaesan Inc.</b>강원도 평창군 진부면 동산리<br/>
+            <span>대표: 오대산 백호</span>|<span>사업자 등록번호: 001-01-00001</span>|<span>이메일: ohdaesan@naver.com</span><br/>
+            <span>대표번호: 02-0000-0000</span>|<span>팩스번호: 02-0000-0000</span><br/>
+            <a href="/termsOfUse" id="termsOfUse"><b>이용약관</b></a>|<a href="/privacyPolicy" id="privacyPolicy"><b>개인정보처리방침</b></a><br/>
+            <b style={{fontSize: "x-small"}}>Copyright ©️ Ohdaesan Inc. All Rights Reserved.</b>
+        </div>
     );
 }
 
