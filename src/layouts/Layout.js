0c313acb088604d9aa372f43ada9e9a6c8c6dbca
DESKTOP-RHTH2UM\80419 <lks609591@gmail.com>
2024-09-12 10:51:06 +0900

    [FEAT] Navbar_Mypage 작성 Navbar_Mypage 작성 완료

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -3,11 +3,13 @@ import Header from "../components/commons/Header"
 import Footer from "../components/commons/Footer"
 import "./Layout.css"
 
+
 function Layout() {
     return (
         <div className="layout">
             <Header/>
             <main className="content">
+
                 <Outlet/>
             </main>
             <Footer/>
